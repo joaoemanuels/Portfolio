873789bb4d281782725f4655e8faf55940f43a22
joaoemanuels <jemanuel.pi@gmail.com>
2026-03-05 00:50:39 -0300

    feat: adds new project entry and updates project data

--- src/components/sections/ProjectSection/index.jsx
+++ src/components/sections/ProjectSection/index.jsx
@@ -6,61 +6,61 @@ import ProjectModal from "../../ui/ProjectModal";
 import "./project-section.styles.css";
 
 function ProjectSection() {
-  const [selectedProject, setSelectedProject] = useState(null);
+	const [selectedProject, setSelectedProject] = useState(null);
 
-  function handleOpen(project) {
-    setSelectedProject(project);
-  }
+	function handleOpen(project) {
+		setSelectedProject(project);
+	}
 
-  function handleClose() {
-    setSelectedProject(null);
-  }
+	function handleClose() {
+		setSelectedProject(null);
+	}
 
-  return (
-    <section id="projects" className="project-section">
-      <div className="project-container">
-        <HeadingSection>Projetos</HeadingSection>
+	return (
+		<section id="projects" className="project-section">
+			<div className="project-container">
+				<HeadingSection>Projetos</HeadingSection>
 
-        <div className="project-highlight">
-          <h3 className="highlight-title">Destaques</h3>
+				<div className="project-highlight">
+					<h3 className="highlight-title">Destaques</h3>
 
-          <div className="project-grid">
-            {projects.map((project) => (
-              <div
-                key={project.id}
-                className="project-card-container"
-                onClick={() => handleOpen(project)}
-              >
-                <div className="project-card">
-                  <img
-                    src={project.images[0]}
-                    alt={project.title}
-                    className="project-image"
-                  />
-                  <div className="project-overlay">
-                    <h4>{project.title}</h4>
+					<div className="project-grid">
+						{projects.map((project) => (
+							<div
+								key={project.id}
+								className="project-card-container"
+								onClick={() => handleOpen(project)}
+							>
+								<div className="project-card">
+									<img
+										src={project.images[0]}
+										alt={project.title}
+										className="project-image"
+									/>
+									<div className="project-overlay">
+										<h4>{project.title}</h4>
 
-                    <div className="tech-stack">
-                      {project.techs.map((tech, index) => (
-                        <img key={index} src={tech} alt="" />
-                      ))}
-                    </div>
-                  </div>
-                </div>
+										<div className="tech-stack">
+											{project.techs.map((tech, index) => (
+												<img key={index} src={tech} alt="" />
+											))}
+										</div>
+									</div>
+								</div>
 
-                <div className="project-footer">
-                  <p>{project.date}</p>
-                  <button type="button">Detalhes</button>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
+								<div className="project-footer">
+									<p>{project.date}</p>
+									<button type="button">Detalhes</button>
+								</div>
+							</div>
+						))}
+					</div>
+				</div>
+			</div>
 
-      <ProjectModal project={selectedProject} onClose={handleClose} />
-    </section>
-  );
+			<ProjectModal project={selectedProject} onClose={handleClose} />
+		</section>
+	);
 }
 
 export default ProjectSection;
--- src/data/projects.js
+++ src/data/projects.js
@@ -1,4 +1,7 @@
 import react from "@/assets/icons/react.svg";
+import html from "@/assets/icons/html5.svg";
+import css from "@/assets/icons/css3.svg";
+import js from "@/assets/icons/js.svg";
 import sass from "@/assets/icons/scss.svg";
 import figma from "@/assets/icons/figma.svg";
 import vercel from "@/assets/icons/vercel.svg";
@@ -7,50 +10,53 @@ import projeto1 from "@/assets/images/projeto1.png";
 import projeto2 from "@/assets/images/projeto2.png";
 import projeto2_1 from "@/assets/images/projeto2_1.png";
 import projeto2_2 from "@/assets/images/projeto2_2.png";
+import projeto3 from "@/assets/images/projeto3.png";
 
 export const projects = [
-  {
-    id: 1,
-    title: "Agenda Pro",
-    date: "10 de Fevereiro de 2026",
+	{
+		id: 1,
+		title: "Agenda Pro",
+		date: "Em desenvolvimento...",
 
-    shortDescription: "Sistema de agendamento para personal trainer.",
+		shortDescription: "Sistema de agendamento para personal trainer.",
 
-    fullDescription: `
+		fullDescription: `
     O Agenda Pro é um sistema completo de agendamento online
     desenvolvido para personal trainers gerenciarem seus alunos,
     horários e sessões de treino de forma prática e organizada.
 
     O projeto foi construído com foco em performance, UX
-    e arquitetura escalável utilizando React e SCSS.
+    e arquitetura escalável utilizando React.
 
     Funcionalidades:
     - Página Landing page
     - Agendamento de planos
-    - Controle de horários disponíveis
     - Painel administrativo
+    - Autenticação segura (admin)
+
+    Em breve: 
+    - Controle de horários disponíveis
     - Gestão de alunos
     - Confirmação de agendamento
-    - Autenticação segura (admin)
     `,
 
-    video: "https://www.youtube.com/embed/pVWxjwhSx_IY",
+		video: "https://www.youtube.com/embed/pVWxjwhSx_IY",
 
-    images: [projeto2, projeto2_1, projeto2_2],
+		images: [projeto2, projeto2_1, projeto2_2],
 
-    techs: [react, sass, figma],
+		techs: [react, sass, figma],
 
-    liveUrl: "https://agenda-pro-cyan-iota.vercel.app/",
-    githubUrl: "https://github.com/joaoemanuels/AgendaPro",
-  },
-  {
-    id: 2,
-    title: "MoneyFlow",
-    date: "11 de Fevereiro de 2026",
+		liveUrl: "https://agenda-pro-cyan-iota.vercel.app/",
+		githubUrl: "https://github.com/joaoemanuels/AgendaPro",
+	},
+	{
+		id: 2,
+		title: "MoneyFlow",
+		date: "11 de Fevereiro de 2026",
 
-    shortDescription: "Sistema de controle financeiro.",
+		shortDescription: "Sistema de controle financeiro.",
 
-    fullDescription: `
+		fullDescription: `
       O MoneyFlow é um sistema completo de controle financeiro
       desenvolvido para ajudar usuários a organizarem receitas,
       despesas e metas financeiras.
@@ -64,13 +70,45 @@ export const projects = [
       - Dashboard com gráficos
     `,
 
-    video: "https://www.youtube.com/embed/pVWxjhSx_IY",
+		video: "https://www.youtube.com/embed/pVWxjhSx_IY",
 
-    images: [projeto1],
+		images: [projeto1],
 
-    techs: [react, sass, figma, vercel],
+		techs: [react, sass, figma, vercel],
 
-    liveUrl: "https://moneyflow.site/",
-    githubUrl: "https://github.com/usuario/moneyflow",
-  },
+		liveUrl: "https://moneyflow.site/",
+		githubUrl: "https://github.com/joaoemanuels/MoneyFlow",
+	},
+	{
+		id: 3,
+		title: "Checklist",
+		date: "11 de Fevereiro de 2026",
+
+		shortDescription: "Sistema de controle financeiro.",
+
+		fullDescription: `
+      O Checklist é um sistema de organização de estudos
+      desenvolvido *para estudo* com foco em ajudar usuários a planejarem,
+      acompanharem e gerenciarem suas tarefas de estudo.
+
+      O projeto foi construído para teste de lógica, com intuito de foco em produtividade,
+      experiência do usuário e organização de rotinas,
+      utilizando React.
+
+      Funcionalidades:
+      - Criação e gerenciamento de tarefas de estudo
+      - Marcação de tarefas concluídas
+      - Organização por prioridades
+      - Interface simples e intuitiva
+    `,
+
+		video: "https://www.youtube.com/embed/1sRwpb4ShA4",
+
+		images: [projeto3],
+
+		techs: [html, css, js, vercel],
+
+		liveUrl: "https://checklist-ruddy-six.vercel.app/",
+		githubUrl: "https://github.com/joaoemanuels/checklist",
+	},
 ];
