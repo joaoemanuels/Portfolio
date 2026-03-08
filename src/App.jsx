603ed0fafc865211dbfbf5f7f36252ed958db48e
joaoemanuels <jemanuel.pi@gmail.com>
2026-03-08 16:30:19 -0300

    fix insights

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/index.jsx";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 function App() {
 	return (
