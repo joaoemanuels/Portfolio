6096b4d3aa430b966bde1c3de8453f568dd7abd2
joaoemanuels <jemanuel.pi@gmail.com>
2026-03-08 16:28:20 -0300

    feat: adds Vercel analytics and speed insights integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/index.jsx";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<AppRoutes />
+			<Analytics />
+			<SpeedInsights />
+		</BrowserRouter>
+	);
 }
 
 export default App;
